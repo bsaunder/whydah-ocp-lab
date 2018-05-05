f913bb140c1e69ac5f48b27212e8a7c95202f8f9
Bryan Saunders <btsaunde@gmail.com>
2018-05-05 18:02:59 -0400

    More Changes to Step 4

--- 3-keys.sh
+++ 3-keys.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "--> Loading Configuration..."
 source ./env.sh
 
+echo "--> Starting Key Copy..."
 for i in `cat hosts|grep -v \\\\[`;
-do 
+do
+	echo "--> Copying Key to $i..."
 	ssh-copy-id root@$i
+	echo "--> Copying Key to $i..."
 done
-
+echo "--> Key Copy Complete"
